0cdddc0bb986bb554952748505014ea5bff676ca
Paulo Carneiro <paulotrc@gmail.com>
2023-12-17 16:53:43 -0300

    [NEW] Primeiro commit

---
+++ svcVendaValidador/src/main/java/br/paulotrc/svcvendavalidador/SvcVendaValidadorApplication.java
@@ -0,0 +1,13 @@
+package br.paulotrc.svcvendavalidador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvcVendaValidadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvcVendaValidadorApplication.class, args);
+	}
+
+}
---
+++ svcVendaValidador/src/test/java/br/paulotrc/svcvendavalidador/SvcVendaValidadorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.paulotrc.svcvendavalidador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvcVendaValidadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
